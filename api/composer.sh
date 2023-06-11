8ee95e85fd728a6e01125afb6f1252e6532a8e55
rajkovrga <rajkovrga@yahoo.com>
2023-06-11 19:17:15 +0200

    added docker compose

---
+++ api/composer.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+docker run \
+  -ti \
+  --rm \
+  --volume "$SSH_AUTH_SOCK:/ssh-auth.sock" \
+  --volume "${COMPOSER_HOME:-$HOME/.composer}:/tmp" \
+  --volume "$PWD:/app" \
+  --env SSH_AUTH_SOCK=/ssh-auth.sock \
+  --env COMPOSER_HOME \
+  --env COMPOSER_CACHE_DIR \
+  --user "$(id -u):$(id -g)" \
+   composer:2.1 "$@"
